44172afd4011d6a7f89ed8c946b076ada38faadb
LeonardoLimaG <leonardolimagarcia5@gmail.com>
2022-08-22 17:10:38 -0300

    segundo commit teste

--- ProjetoBase/Controllers/HomeController.cs
+++ ProjetoBase/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace ProjetoBase.Controllers
         {
             return View();
         }
+        public IActionResult Receitas()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
