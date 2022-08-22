52aab38327fc64489bc91813cea251794da75786
LeonardoLimaG <leonardolimagarcia5@gmail.com>
2022-08-22 20:57:08 -0300

    Criação de paginas e rootas (Pagina de criar despesa e controller)

---
+++ ProjetoBase/Controllers/DespesaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoBase.Models;
+using System.Diagnostics;
+
+namespace ProjetoBase.Controllers
+{
+    public class DespesaController : Controller
+    {
+        public IActionResult CriarDespesa()
+        {
+            return View();
+        }
+    }
+}
