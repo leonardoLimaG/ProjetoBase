847cbed3193ca5eba084ed663dcc303f47a419c2
LeonardoLimaG <leonardolimagarcia5@gmail.com>
2022-08-22 17:37:19 -0300

    terceito commit

--- ProjetoBase/Controllers/HomeController.cs
+++ ProjetoBase/Controllers/HomeController.cs
@@ -22,10 +22,6 @@ namespace ProjetoBase.Controllers
         {
             return View();
         }
-        public IActionResult Receitas()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
